f33bdf3ac14e88d25feea17949686b0f516f7007
Elena Williams <ele.wil@gmail.com>
2015-05-09 02:59:37 +1000

    Add os environ secret key.

--- project/settings.py
+++ project/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
